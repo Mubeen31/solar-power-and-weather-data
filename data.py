0871294655fb46880444fad9f6094d25188c18a6
Mubeen31 <qs6272527@gmail.com>
2022-10-03 00:01:49 +0100

    first commit

--- data.py
+++ data.py
@@ -10,12 +10,33 @@ ser = serial.Serial(PORT, 9600)
 while True:
     message = ser.readline()
     data = message.strip().decode()
-    split_string = data.split(',')  # split string
-    voltage = split_string[0]  # convert first part of string into float
-    current = split_string[1]  # convert second part of string into float
-    now = datetime.now()
-    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-    with open("sensors_data.csv", "a", newline = '\n') as f:
-        writer = csv.writer(f, delimiter = ",")
-        writer.writerow([dt_string, voltage, current])
-        print(dt_string, voltage, current)
+    char_length = len(data)
+    if char_length == 17:
+        split_string = data.split(',')  # split string
+        voltage = split_string[0]  # convert first part of string into float
+        current = split_string[1]  # convert second part of string into float
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open("sensors_data.csv", "a", newline = '\n') as f:
+            writer = csv.writer(f, delimiter = ",")
+            writer.writerow([dt_string, voltage, current])
+            print(dt_string, voltage, current)
+    elif char_length == 18:
+        split_string = data.split(',')  # split string
+        voltage = split_string[0]  # convert first part of string into float
+        current = split_string[1]  # convert second part of string into float
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open("sensors_data.csv", "a", newline = '\n') as f:
+            writer = csv.writer(f, delimiter = ",")
+            writer.writerow([dt_string, voltage, current])
+            print(dt_string, voltage, current)
+    elif char_length >= 19:
+        voltage = float(0.00000)  # convert first part of string into float
+        current = float(0.00000)  # convert second part of string into float
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open("sensors_data.csv", "a", newline = '\n') as f:
+            writer = csv.writer(f, delimiter = ",")
+            writer.writerow([dt_string, voltage, current])
+            print(dt_string, voltage, current)
