99dc17b209fc895b876da66a75e15d3876fdc3b2
Mubeen Ali <qs6272527@gmail.com>
2022-10-03 15:32:19 +0100

    first commit

--- data.py
+++ data.py
@@ -17,8 +17,8 @@ while True:
         current = split_string[1]  # convert second part of string into float
         now = datetime.now()
         dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-        with open("sensors_data.csv", "a", newline = '\n') as f:
-            writer = csv.writer(f, delimiter = ",")
+        with open("sensors_data.csv", "a", newline='\n') as f:
+            writer = csv.writer(f, delimiter=",")
             writer.writerow([dt_string, voltage, current])
             print(dt_string, voltage, current)
     elif char_length == 18:
@@ -27,8 +27,8 @@ while True:
         current = split_string[1]  # convert second part of string into float
         now = datetime.now()
         dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-        with open("sensors_data.csv", "a", newline = '\n') as f:
-            writer = csv.writer(f, delimiter = ",")
+        with open("sensors_data.csv", "a", newline='\n') as f:
+            writer = csv.writer(f, delimiter=",")
             writer.writerow([dt_string, voltage, current])
             print(dt_string, voltage, current)
     elif char_length >= 19:
@@ -36,7 +36,34 @@ while True:
         current = float(0.00000)  # convert second part of string into float
         now = datetime.now()
         dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-        with open("sensors_data.csv", "a", newline = '\n') as f:
-            writer = csv.writer(f, delimiter = ",")
+        with open("sensors_data.csv", "a", newline='\n') as f:
+            writer = csv.writer(f, delimiter=",")
+            writer.writerow([dt_string, voltage, current])
+            print(dt_string, voltage, current)
+    elif char_length < 17:
+        voltage = float(0.00000)  # convert first part of string into float
+        current = float(0.00000)  # convert second part of string into float
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open("sensors_data.csv", "a", newline='\n') as f:
+            writer = csv.writer(f, delimiter=",")
+            writer.writerow([dt_string, voltage, current])
+            print(dt_string, voltage, current)
+    elif char_length is None:
+        voltage = float(0.00000)  # convert first part of string into float
+        current = float(0.00000)  # convert second part of string into float
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open("sensors_data.csv", "a", newline='\n') as f:
+            writer = csv.writer(f, delimiter=",")
+            writer.writerow([dt_string, voltage, current])
+            print(dt_string, voltage, current)
+    elif char_length == '':
+        voltage = float(0.00000)  # convert first part of string into float
+        current = float(0.00000)  # convert second part of string into float
+        now = datetime.now()
+        dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open("sensors_data.csv", "a", newline='\n') as f:
+            writer = csv.writer(f, delimiter=",")
             writer.writerow([dt_string, voltage, current])
             print(dt_string, voltage, current)
